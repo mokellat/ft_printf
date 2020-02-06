7b7f5ab33b9efcd6c0ae7e2aae0e106cf3f25315
mokellat <fsociety103@gmail.com>
2020-02-07 00:00:13 +0100

    new

--- find_conversion.c
+++ find_conversion.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   find_conversion.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mokellat <mokellat@student.42.fr>          +#+  +:+       +#+        */
+/*   By: medali <medali@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/08 13:00:36 by mokellat          #+#    #+#             */
-/*   Updated: 2020/02/02 21:49:36 by mokellat         ###   ########.fr       */
+/*   Updated: 2020/02/06 23:18:34 by medali           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,12 @@ void	find_conversion(const char *ptr, va_list ap, flagcheck flags)
 		ft_printf_c(ptr, ap, flags);
 	else if (flags.conversion == 'i')
 		ft_printf_flag_reexecution(ptr, ap, flags);
-	else if (flags.conversion == 'p')
-		ft_printf_p(ptr, ap, flags);
 	else if (flags.conversion == 'x')
 		ft_printf_x(ptr, ap, flags);
 	else if (flags.conversion == 'X')
 		ft_printf_x(ptr, ap, flags);
+	else if (flags.conversion == 'p')
+		ft_printf_p(ptr, ap, flags);
 	/*else if (flags.conversion == 'u')
 		ft_printf_u(ptr, ap);*/
 }
--- main.c
+++ main.c
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mokellat <mokellat@student.42.fr>          +#+  +:+       +#+        */
+/*   By: medali <medali@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/30 18:34:53 by mokellat          #+#    #+#             */
-/*   Updated: 2020/02/02 23:01:01 by mokellat         ###   ########.fr       */
+/*   Updated: 2020/02/06 23:15:41 by medali           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 #define TEST ("%.10d",10)
 #include <limits.h>
-#define test1 ("%x", 100)
+#define test1 ("%p","kellati")
 int main()
 {
 	int i;
